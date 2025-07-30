b0ce3756bbb2d1044e558495d6231ddbe8a179f1
Hgazzar <97703059+Hgazzar@users.noreply.github.com>
2025-07-30 07:35:35 +0300

    Add .next/static/chunks/src_app_page_tsx_6e0afc73._.js 1753850135004

---
+++ .next/static/chunks/src_app_page_tsx_6e0afc73._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_85f0a7c8._.js",
+  "static/chunks/node_modules_c998cb3a._.js"
+],
+    source: "dynamic"
+});
